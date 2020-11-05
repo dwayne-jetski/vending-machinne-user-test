4e2ac7e720fe5d5512462f61e40706a5e615b192
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-04 20:13:49 -0600

    added user tests for calculateReturnChange function to vendingMachineSpec.js for each object in source file, vending-machine.js

--- spec/vendingMachineSpec.js
+++ spec/vendingMachineSpec.js
@@ -1,4 +1,16 @@
 describe("vendingMachine", function(){
 
+    //tests the calculateReturnChange on individual item prices
+it("should calculate the proper return change for candyBar", function(){
+   
+    expect(vendingMachine.calculateReturnChange(vendingMachine.candyBars[0].price, vendingMachine.amountOfMoneyInChange)).toBe(4.25);
+ });
+ it("should calculate the proper return change for bagOfChips", function(){
     
+     expect(vendingMachine.calculateReturnChange(vendingMachine.bagsOfChips[0].price, vendingMachine.amountOfMoneyInChange)).toBe(4.11);
+  });
+  it("should calculate the proper return change for packOfGum", function(){
+    
+     expect(vendingMachine.calculateReturnChange(vendingMachine.packsOfGum[0].price, vendingMachine.amountOfMoneyInChange)).toBe(4.85);
+  });
 })
